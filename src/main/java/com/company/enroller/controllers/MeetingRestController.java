17091e174a46437373ed5a2cce7dd971069331bb
midudzik <midudzik@d17-427-pc-17.ipa.ii.agh.edu.pl>
2024-04-27 11:47:39 +0200

    add adding deleting and updating of the meetings

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -34,4 +34,36 @@ public class MeetingRestController {
 		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> addParticipant(@RequestBody Meeting meeting) {
+		if (meetingService.findById(meeting.getId()) != null) {
+			return new ResponseEntity<String>(
+					"Unable to create. A meeting with id " + meeting.getId() + " already exist.",
+					HttpStatus.CONFLICT);
+		}
+		meetingService.add(meeting);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable("id") long id) {
+		Meeting meeting = meetingService.findById(id);
+		if (meeting == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		meetingService.delete(meeting);
+		return new ResponseEntity<Meeting>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Meeting updatedMeeting) {
+		Meeting meeting = meetingService.findById(id);
+		if (meeting == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		meeting.setTitle(updatedMeeting.getTitle());
+		meetingService.update(meeting);
+		return new ResponseEntity<Participant>(HttpStatus.OK);
+	}
+
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -3,6 +3,7 @@ package com.company.enroller.persistence;
 import com.company.enroller.model.Meeting;
 import com.company.enroller.model.Participant;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -38,6 +39,25 @@ public class MeetingService {
 
 	public Meeting findById(long id) {
 		return session.get(Meeting.class, id);
+	}
+
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = session.beginTransaction();
+		session.merge(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = session.beginTransaction();
+		session.delete(meeting);
+		transaction.commit();
 	}
 
 }
